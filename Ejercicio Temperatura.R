66d7613a31c1b57b8fe93f4e1544d8a37eaaf1d2
RamonPosgrado <Rcopadog@uanl.edu.mx>
2025-08-28 09:50:30 -0600

    ahora si ya quedo

--- Ejercicio Temperatura.R
+++ Ejercicio Temperatura.R
@@ -46,17 +46,15 @@ boxplot(temp10, col = colores,
 # Importar datos web 
 
 
-url <-"https://repodatos.atdt.gob.mx/api_update/senasica
-/actividades_inspeccion_movilizacion/29_actividades-inspeccion
--movilizacion.csv"
+url <- "https://repodatos.atdt.gob.mx/api_update/senasica/actividades_inspeccion_movilizacion/29_actividades-inspeccion-movilizacion.csv"
 
 url2 <- paste0("https://repodatos.atdt.gob.mx/api_update/",
                "senasica/actividades_inspeccion_movilizacion/",
                "29_actividades-inspeccion-movilizacion.csv")
 
-senasica <- read.csv (url, header = T)
+senasica <- read.csv(url, header = T)
 
-senasica2 <- read.csv (url2, header = T)
+senasica2 <- read.csv(url2, header = T)
 head (senasica2[, c(1,3:12)])
 
 View (senasica)
