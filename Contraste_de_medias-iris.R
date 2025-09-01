6d72832793351c8111cc6ff7c6f62e5f5c732408
Ramon Copado <rcopadog@uanl.edu.mx>
2025-09-01 11:55:51 -0600

    otro avance

--- Contraste_de_medias-iris.R
+++ Contraste_de_medias-iris.R
@@ -109,6 +109,40 @@ table(data_sub$variety)
     
   #Plantee formalmente las hipótesis estadísticas para una prueba t de dos 
   #muestras independientes (two.sided).
+      # Observar datos
+       # Aplicar subconjunto para cada tratamiento
+    
+    df_Versicolor <- subset(data_sub, variety == "Versicolor")
+    df_Virginica <- subset(data_sub, variety == "Virginica")
+    par(mfrow=c(1,2))
+    qqnorm(df_Versicolor$petal.length); qqline(df_Versicolor$petal.length)
+    qqnorm(df_Virginica$petal.length); qqline(df_Virginica$petal.length)
+    par(mfrow=c(1,1))
+    
+    
+      # Prueba normalidad SHAPIRO.TEST
+        shapiro.test(df_Versicolor$petal.length)
+        shapiro.test(df_Virginica$petal.length)
+
+      # Revisar homogeneidad
+        var.test(data_sub$petal.length ~ data_sub$variety)
+
+
+      # Prueba de T
+
+        t.test(data_sub$petal.length ~ data_sub$variety, alternative = "two.sided",
+               var.equal = T)
+        t.test(data_sub$petal.length ~ data_sub$variety, alternative = "two.sided", 
+               var.equal = F)
+
+          # Error estadistico de como plantear la pregunta
+            t.test(data_sub$petal.length ~ data_sub$variety, alternative = "greater", 
+                   var.equal = T)
+            t.test(data_sub$petal.length ~ data_sub$variety, alternative = "greater", 
+                   var.equal = F)
+
+
+    
     
     
   #Ejecute la prueba en R justificando el tipo de prueba (Welch cuando las 
@@ -117,6 +151,18 @@ table(data_sub$variety)
     
     
   #Calcule e interprete el tamaño del efecto (Cohen’s d)
+            # Medir el efecto del efecto
+            
+            cohens_efecto <- function(x,y) {
+              n1 <- length(x); n2 <- length(y)
+              s1 <- sd(x); s2<-sd(y)
+              sp <- sqrt(((n1 - 1) * s1^2 + (n2 - 1) * s2^2) / (n1 + n2 - 2))
+              (mean (x) - mean (y)) / sp
+            }
+            
+            d1_cal <- cohens_efecto(df_ctrl$IE, df_fert$IE)
+            d1_cal
+            
 
 #Visualización
     # Genere una gráfica comparativa (boxplot, violinplot, etc.) que muestre 
@@ -128,3 +174,4 @@ table(data_sub$variety)
       #• Planteamiento del problema y de las hipótesis.
       #• Resultados numéricos y gráficos.
       #• Interpretación estadística y biológica.
+    
