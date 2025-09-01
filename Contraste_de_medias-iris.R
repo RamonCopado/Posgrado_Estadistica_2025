1ff0764874d78ae8b50b7462d402dc4d644b7513
Ramon Copado <rcopadog@uanl.edu.mx>
2025-09-01 11:29:48 -0600

    nuevo avance

--- Contraste_de_medias-iris.R
+++ Contraste_de_medias-iris.R
@@ -2,18 +2,26 @@
 ##HW_02
 #Ramón Copado Garcia 
 ##Laboratorio2: Contraste de Medias
-##TrabajarcondatosenR
-##Script5
+##Trabajar condatos en R
+##Script 5
 ##30/8/2025
-##RamónCopadoGarcía
-##Matricula1059439
+##Ramón Copado García
+##Matricula 1059439
+
+#Importar datos de Github
 
 url<-("https://gist.githubusercontent.com/netj/8836201/raw/6f9306ad21398ea43cba4f7d537619d0e07d5ae3/iris.csv")
 url2<-paste0("https://gist.githubusercontent.com/netj/8836201/raw/",
               "6f9306ad21398ea43cba4f7d537619d0e07d5ae3/iris.csv")
+
 iris<-read.csv(url,header=T)
+
 iris<-read.csv(url2,header=T)
+
 View (iris)
+
+#Tambien se puede utilizar, data("iris"), y trabajar sobre la base de datos
+
 
 
 # Ejercicio
@@ -22,13 +30,19 @@ View (iris)
 #presenta diferencias en su media poblacional entre estas dos especies.
 head(iris)
 summary(iris)
+dim(iris)
+names(iris)
+str(iris)
+df<-iris[3]
+boxplot(df)
+
 
 iris$variety <- as.factor(iris$variety)
 class(iris$variety)
 
 summary(iris)
 
-mean(Iris$petal_length)
+by(iris[1:4],iris$variety, summary)
 
 tapply(iris$petal.length, iris$variety, mean)
 
@@ -44,14 +58,6 @@ boxplot (iris$petal.length ~ iris$variety, col = colores,
          main = "Distribución del largo de pétalo por especie",
          xlab = "Especie",
          ylab = "Largo de pétalo")
-
-#Lo anterior es solo para visualisar las 150 muestras.
-
-#Datos a trabajar
-# +Selección de especies: elija las especies versicolor y virginica de la base y,
-#  enfoque su análisien la variable Pental.Length
-
-
 df_Versicolor <- subset(iris, variety == "Versicolor")
 df_Virginica <- subset(iris, variety == "Virginica")
 
@@ -60,18 +66,25 @@ qqnorm(df_Versicolor$petal.length); qqline(df_Versicolor$petal.length)
 qqnorm(df_Virginica$petal.length); qqline(df_Virginica$petal.length)
 par(mfrow=c(1,1))
 
-Species<-("Versicolor,Virginica")
+#Lo anterior es solo para visualisar las 150 muestras.
 
-data_sub <-subset(iris, Species %in% c("Versicolor","Virginica"))
+#Datos a trabajar
+# +Selección de especies: elija las especies versicolor y virginica de la base y,
+#  enfoque su análisien la variable Pental.Length
 
-c("Versicolor","Virginica")
-Species %in% c("Versicolor","Virginica")
-subset(iris, "Versicolor","Virginica")
-subset(iris, variety == "Versicolor")
-subset(iris, variety == "Virginica")
-View (Species)
 
-data_sub <-subset(iris, Species %in% c("Versicolor","Virginica"))
+
+#Vamos a introducir el operador%in% para realizar el subset. En R, el
+#operador%in% se utiliza para preguntar si un valor pertenece a un conjunto 
+# de valores. Devuelve un vector lógico (TRUE o FALSE) indicando si cada 
+#elemento de la izquierda está contenido dentro del vector de la derecha.
+
+variety<-("Versicolor,Virginica")
+
+data_sub <-subset(iris, variety %in% c("Versicolor","Virginica"))
+table(data_sub$variety)
+
+
 
 
 
@@ -79,9 +92,39 @@ data_sub <-subset(iris, Species %in% c("Versicolor","Virginica"))
 
 #Primer contacto con R
   #Explorar la base de datos iris usando funciones como head(), Summary()
-head(data_sub)
-summary(data_sub)
+    head(data_sub)
+    summary(data_sub)
 
-#Identificar las variables Petal.Lenght y determina las estadísticas descriptivas
-#para las dos especies.
-continuara
+  #Identificar las variables Petal.Length y determina las estadísticas descriptivas
+  #para las dos especie
+    tapply(data_sub$petal.length, data_sub$variety, summary)
+    tapply(data_sub$petal.length, data_sub$variety, mean)
+    tapply(data_sub$petal.length, data_sub$variety, sd)
+    tapply(data_sub$petal.length, data_sub$variety, var)
+    by(data_sub[3], data_sub$variety,summary)
+
+#Prueba estadística
+  #Defina una pregunta de invertigación sobre la variable Petal.Length
+  
+    
+  #Plantee formalmente las hipótesis estadísticas para una prueba t de dos 
+  #muestras independientes (two.sided).
+    
+    
+  #Ejecute la prueba en R justificando el tipo de prueba (Welch cuando las 
+  #varianzas son diferentes o clásica, cuando las varainzas son iguales).
+    
+    
+    
+  #Calcule e interprete el tamaño del efecto (Cohen’s d)
+
+#Visualización
+    # Genere una gráfica comparativa (boxplot, violinplot, etc.) que muestre 
+    #las diferencias entre especies.
+    
+    
+#Informe escrito:
+    #Redacte una síntesis (máx. 1 cuartilla) que incluya:
+      #• Planteamiento del problema y de las hipótesis.
+      #• Resultados numéricos y gráficos.
+      #• Interpretación estadística y biológica.
