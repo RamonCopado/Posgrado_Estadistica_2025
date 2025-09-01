768d069c7d59e8a2b28bac318280275532f71fd4
Ramon Copado <rcopadog@uanl.edu.mx>
2025-08-31 23:14:12 -0600

    avance

--- Contraste_de_medias-iris.R
+++ Contraste_de_medias-iris.R
@@ -22,3 +22,66 @@ View (iris)
 #presenta diferencias en su media poblacional entre estas dos especies.
 head(iris)
 summary(iris)
+
+iris$variety <- as.factor(iris$variety)
+class(iris$variety)
+
+summary(iris)
+
+mean(Iris$petal_length)
+
+tapply(iris$petal.length, iris$variety, mean)
+
+tapply(iris$petal.length, iris$variety, sd)
+
+tapply(iris$petal.length, iris$variety, var)
+
+colores <-c ("navajowhite", "salmon", "skyblue")
+
+# Crear un boxplot iris
+
+boxplot (iris$petal.length ~ iris$variety, col = colores,
+         main = "Distribución del largo de pétalo por especie",
+         xlab = "Especie",
+         ylab = "Largo de pétalo")
+
+#Lo anterior es solo para visualisar las 150 muestras.
+
+#Datos a trabajar
+# +Selección de especies: elija las especies versicolor y virginica de la base y,
+#  enfoque su análisien la variable Pental.Length
+
+
+df_Versicolor <- subset(iris, variety == "Versicolor")
+df_Virginica <- subset(iris, variety == "Virginica")
+
+par(mfrow=c(1,2))
+qqnorm(df_Versicolor$petal.length); qqline(df_Versicolor$petal.length)
+qqnorm(df_Virginica$petal.length); qqline(df_Virginica$petal.length)
+par(mfrow=c(1,1))
+
+Species<-("Versicolor,Virginica")
+
+data_sub <-subset(iris, Species %in% c("Versicolor","Virginica"))
+
+c("Versicolor","Virginica")
+Species %in% c("Versicolor","Virginica")
+subset(iris, "Versicolor","Virginica")
+subset(iris, variety == "Versicolor")
+subset(iris, variety == "Virginica")
+View (Species)
+
+data_sub <-subset(iris, Species %in% c("Versicolor","Virginica"))
+
+
+
+#Instrucción de tarea
+
+#Primer contacto con R
+  #Explorar la base de datos iris usando funciones como head(), Summary()
+head(data_sub)
+summary(data_sub)
+
+#Identificar las variables Petal.Lenght y determina las estadísticas descriptivas
+#para las dos especies.
+
